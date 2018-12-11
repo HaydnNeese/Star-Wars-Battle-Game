bdeda9f936291dd6478c46bb4067e53da7642013
HaydnNeese <haydnneese@yahoo.com>
2018-12-11 00:10:01 -0600

    more javascript finished

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,19 +3,23 @@ var playerHealth, playerAttack, playerBaseAttack, enemyHealth, enemyAttack;
 
 //choosing player character
 $('body').on("click", '.chooseChar .charCard', function() {
+
     var currentElement = $(this); // assign a currentElement variable to be the image we click on
     $("#playerCharacter").append(currentElement); // send the image to the playerCharacter box
     console.log( $("#playerCharacter"));
+
    $(".enemyRow").append($('.chooseChar .charCard')); //send the remaining characters to the enemy row
    playerHealth = currentElement.data("health"); //retrieve the health of the chosen character from the HTML pages value
    playerAttack = currentElement.data("attackPower"); //retrieve the attackpower of the chosen character from the HTML page's value to be dynamically changed from character's base attack value 
    playerBaseAttack = currentElement.data("attackPower"); //retrieve the attackpower of the chosen character from the HTML page's value to be the base attack
    updateCharCardDisplay($(currentElement), playerHealth, playerAttack); //update the chosen characters stats when it goes to the fightBox
+
    //console.log(currentElement.data("health"), currentElement.data("attackPower"), currentElement.data("counterPower"));
 });
 
 // when choosing an card in the enemy row send it to opponent fighter box; hide the enemy row; 
 $('body').on("click", '.chooseEnemy .charCard', function() {
+
     var currentElement = $(this); // recognize which card we click
     $("#opponent").append(currentElement); //send it to the enemy box
     $(".chooseEnemy .charCard").hide(); // hide the enemy row during the battle
@@ -25,29 +29,49 @@ $('body').on("click", '.chooseEnemy .charCard', function() {
    // console.log( $("#playerCharacter"));
 });
 //put the retrieved value on the charCard for the user to see
-function updateCharCardDisplay (charCard, health, attack) {
+function updateCharCardDisplay (charCard, health, attack) { 
+
     charCard.find('.health').text('HP: ' + health); //update the health stat on the card
     charCard.find('.attack').text('Attack: ' + attack);//update the attack stat on the card
 }
 
-
-
 $('#attackButton').on('click', function() {
+    var opponentNewHealth = enemyHealth - playerAttack;
+    var increasePlayerDamage = playerAttack + playerBaseAttack;
+    var counterDamage = playerHealth - enemyAttack;    
+
     // attack button when clicked needs to lower health of opponent using the attack power total
-    
+    $('#opponent .health').text('HP: ' + opponentNewHealth);
+
     // when attack power is clicked it will increase by base attack power's value
+    $('#playerCharacter .attack').text('Attack: ' + increasePlayerDamage);
 
     // lower player health by opponent's counter attack total
+    $('#playerCharacter .health').text('HP: ' + counterDamage);
 
+    playerHealth = counterDamage; //reassign new value to playerHealth
+    playerAttack = increasePlayerDamage; //reassign new value to attackPower
+    //if player HP reaches 0: alert gameover, reset
+
+    if(playerHealth <= 0) {
+        alert('Something Something Dark Side');//alert loss
+        document.location.reload();//reload entire game
+
+    }else if(opponentNewHealth <= 0) {
+        alert('Next opponent');//alert new opponent
+        $('.deadOpponent').append($('#opponent .charCard'));//send dead opponenent to the defeated pile
+        $('.chooseEnemy .charCard').show();//show enemy selection again
+    }
 });
 
-//if enemy health = 0 or less then remove image from screen 
-//alert win round
-//reveal enemy row to user so they can choose a new fighter
-//send new enemy to fightbox
-//redo fighting mechanics
-//and enemy health win condition
-//if all three enemies die then alert victory of game
-//if player health = 0 change image to (some death image) and alert loss message
-//create a reset button and make the reset function
+//if all three enemies die then alert victory of game and reset entire game
 
+if('.deadOpponent'.document.getElementsByClassName='.charCard'.length <= 3) {
+    alert('The force is strong in this one');
+    document.location.reload();
+}//still not working
+
+//create a reset button and make the reset function
+$('#restartButton').on('click', function() {
+    document.location.reload();
+})
